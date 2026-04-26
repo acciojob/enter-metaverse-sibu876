d10e1f086013b969421775df724b5361a4fbc70d
Sibghatullah <145217999+sibu876@users.noreply.github.com>
2026-04-26 23:15:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const parag = document.getElementById("status");
+const btn = document.getElementById("enterBtn");
+btn.onclick  = () => {
+	const heading = document.createElement("h1");
+	heading.textContent = "Entered Metaverse";
+	parag.replaceWith(heading);
+    
+}
