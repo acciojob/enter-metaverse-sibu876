7fc90e3fd1776686bab9419b51d3e4082419d68f
Sibghatullah <145217999+sibu876@users.noreply.github.com>
2026-04-26 23:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 const parag = document.getElementById("status");
 const btn = document.getElementById("enterBtn");
 btn.onclick  = () => {
-	const heading = document.createElement("h1");
-	heading.textContent = "Entered Metaverse";
-	parag.replaceWith(heading);
+	 const oldEl = document.getElementById("status");
+
+    const newEl = document.createElement("h1");
+    newEl.id = "status"; // keep same id
+    newEl.textContent = "Entered Metaverse";
+
+    oldEl.replaceWith(newEl);
     
 }
